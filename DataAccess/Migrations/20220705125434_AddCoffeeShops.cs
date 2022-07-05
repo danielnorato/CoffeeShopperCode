b146d83e3ba76b3449c8f4a01ac8340d13b024b1
Daniel Souza <daniel@xbits.com.br>
2022-07-05 17:16:27 -0300

    Adicionar arquivos de projeto.

---
+++ DataAccess/Migrations/20220705125434_AddCoffeeShops.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AddCoffeeShops : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops(Name,OpeningHours,Address) VALUES('Starbuck paranagua','8-5 Segunda-sabado','Rua Alvorada 3923')");
+
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops(Name,OpeningHours,Address) VALUES('Starbuck Curitiba','10-11 Segunda-sabado','Rua Ibipora 25')");
+
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops(Name,OpeningHours,Address) VALUES('Starbuck Pato Branco','7-8 Segunda-sabado','Rua Matriz 800')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
