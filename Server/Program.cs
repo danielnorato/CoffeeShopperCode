8964fd454d9f120a90f34d63f8aa17d87fe22b2e
Daniel Souza <daniel@xbits.com.br>
2022-07-06 17:18:48 -0300

    Segundo Push

--- Server/Program.cs
+++ Server/Program.cs
@@ -43,10 +43,10 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
-app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapDefaultControllerRoute();
-});
+//app.UseAuthorization();
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapDefaultControllerRoute();
+//});
 
 app.Run();
